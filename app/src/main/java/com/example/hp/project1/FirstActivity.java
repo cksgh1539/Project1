b9b9fc40c2a50936dd1231dd28fbfb7d3f903f1c
yoongaeun <31908594+yoongaeun@users.noreply.github.com>
2017-10-17 10:14:27 +0900

    평점,Rice/Fry받아오기
    시간되면 디자인까지~

--- app/src/main/java/com/example/hp/project1/FirstActivity.java
+++ app/src/main/java/com/example/hp/project1/FirstActivity.java
@@ -2,6 +2,7 @@ package com.example.hp.project1;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -23,8 +24,7 @@ public class FirstActivity extends AppCompatActivity {
 
         String[] items = {"Roll/Noodles", "Rice", "Fry" };
 
-        ArrayAdapter<String> adapt
-                = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+        ArrayAdapter<String> adapt = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
         return adapt;
     }
     @Override
@@ -37,11 +37,6 @@ public class FirstActivity extends AppCompatActivity {
 
         ListView list = (ListView) findViewById(R.id.ListView1);
         list.setAdapter(adapt);
-
-        list.setDivider(new ColorDrawable(Color.GRAY));
-        list.setDividerHeight(5);
-
-
 
         list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View vClicked,
@@ -56,12 +51,11 @@ public class FirstActivity extends AppCompatActivity {
                     Intent intent = new Intent(getApplicationContext(), Fry.class);
                     startActivity(intent);
                 }
-
             }
 
         });
-
     }
+
     @Override
     protected void onStart() {
         super.onStart();
@@ -85,70 +79,17 @@ public class FirstActivity extends AppCompatActivity {
         super.onStop();
         Log.i(TAG, getLocalClassName() + ".onStop");
     }
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
         Log.i(TAG, getLocalClassName() + ".onDestroy");
     }
 
+    public void calling(View v) {
+        Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:027604499"));
+        startActivity(intent);
+    }
+
 }
-    /*ArrayList<Restaurant1> data = new ArrayList<>();
-        data.add(new Restaurant1("Roll/Noodles ", "Rice", "Fry"));
 
-        final MyAdapter adapter = new MyAdapter(data, this, R.layout.item);
-
-        ListView listView = (ListView) findViewById(R.id.listView);
-        listView.setAdapter(adapter);
-        listView.setDividerHeight(5);*/
-       /* list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View vClicked,
-                                    int position, long id) {
-                Intent intent = new Intent(getApplicationContext(), noodle.class);
-                startActivity(intent);
-            }
-        });*/
-
-/*    public void button1(View view) {
-        Intent intent = new Intent(getApplicationContext(), noodle.class);
-        startActivity(intent);
-    }
-    public void button2(View view) {
-        Intent intent = new Intent(getApplicationContext(), noodle.class);
-        startActivity(intent);
-    }
-    public void button3(View view) {
-        Intent intent = new Intent(getApplicationContext(), Fry.class);
-        startActivity(intent);
-    }*/
-
-
-
-    /*@Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.main_menu, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-*/
-   /* @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.quick_action1:
-                Toast.makeText(getApplicationContext(), "quick", Toast.LENGTH_SHORT).show();
-                return true;
-            case R.id.action_settings:
-                Toast.makeText(getApplicationContext(), "setting", Toast.LENGTH_SHORT).show();
-                return true;
-            case R.id.action_subactivity:
-                startActivity(new Intent(this, Fry.class));
-
-                Intent intent = new Intent(getApplicationContext(), Fry.class);
-                intent.putExtra("string_msg", "Hello");
-                startActivityForResult(intent, 0);
-
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
-*/
